740ba5c1d73e788791672710db0c4babe34af66c
Henry Chochu <hisnameishenry01@gmail.com>
2025-06-06 16:27:03 +0300

    Added data verification for user model

--- thami_backend/app/models/user.rb
+++ thami_backend/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  enum role: { customer: 0, admin: 1 }
+
+  validates :name, presence: true
+  validates :phone_number, presence: true, uniqueness: true
+  validates :email, uniqueness: true, allow_blank: true
 end
