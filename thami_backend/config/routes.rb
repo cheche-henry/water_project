78c288c8b69be01277a02e0621368390521c7b3a
Henry Chochu <hisnameishenry01@gmail.com>
2025-06-07 11:09:48 +0300

    Users controller

--- thami_backend/config/routes.rb
+++ thami_backend/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "users/index"
+  get "users/show"
+  get "users/destroy"
   get "sessions/create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
